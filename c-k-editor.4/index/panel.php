0f0d563434f8ed94f2bbbb4c38e1c125ede84696
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-06-03 10:56:12 +0700

    Update

--- c-k-editor.4/index/panel.php
+++ c-k-editor.4/index/panel.php
@@ -2,10 +2,7 @@
 
 require __DIR__ . DS . '..' . DS . 'engine' . DS . 'r' . DS . 'asset.php';
 
-if (
-    isset($_GET['layout']) && 0 === strpos($_GET['layout'] . '.', 'page.') ||
-    ($f = $_['f']) && false !== strpos(',archive,draft,page,', ',' . pathinfo($f, PATHINFO_EXTENSION) . ',')
-) {
+if (0 === strpos($_['layout'] . '.', 'page.')) {
     require __DIR__ . DS . '..' . DS . 'engine' . DS . 'r' . DS . 'hook.php';
     require __DIR__ . DS . '..' . DS . 'engine' . DS . 'r' . DS . 'state.php';
 }
